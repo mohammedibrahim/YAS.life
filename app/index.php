fe96643748dd5b9d1770a538121739af6ed8bf66
Mohamed Ibrahim <m@mibrah.im>
2018-11-30 11:38:39 +0200

    Init app

---
+++ app/index.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * task2.
+ *
+ * @package     task2.
+ * @author      Mohamed Ibrahim <m@mibrah.im>
+ * @version     v.1.0 (30/11/2018)
+ * @copyright   Copyright (c) 2018
+ */
+
+/**
+ * ${DESC}
+ *
+ * Class ${NAME}
+ * @package ${NAMESPACE}
+ */
